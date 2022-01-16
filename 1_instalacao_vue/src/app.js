5560f09b38d29e23502d5cad130d907ea4f3ff72
dvdluiz <dvdluiz1991@gmail.com>
2022-01-16 10:06:26 -0300

    Inputs e Data Binding

--- 1_instalacao_vue/src/app.js
+++ 1_instalacao_vue/src/app.js
@@ -1,9 +1,21 @@
 const MyNameApp= {
     data(){
       return{
-          name:"David",
-          idade: 30
+          name:"",
+          idade: 30,
+          input_name:"" 
       }  
+    },
+    methods:{
+      submitForm(e){
+
+        e.preventDefault();
+
+        console.log(this.input_name);
+
+        this.name = this.input_name;
+
+      }
     }
 }
 
